9694f74f52916dcd56950bfec5ec5584014828ac
FerreWagner <18408229270@163.com>
2018-08-26 23:39:29 +0800

    fixed mysql bug

--- protected/config.php
+++ protected/config.php
@@ -7,7 +7,6 @@ $config = array(
 	'rewrite' => array(
 		'admin/index.html' => 'admin/main/index',
 		'admin/<c>_<a>.html'    => 'admin/<c>/<a>', 
-        	'<m>/<c>/<a>'          => '<m>/<c>/<a>',
 		'<c>/<a>'          => '<c>/<a>',
 		'/'                => 'main/index',
 	),
@@ -28,11 +27,18 @@ $domain = array(
 		),
 	),
 	"simplespeed.com" => array( //线上配置
-		'debug' => 0,
-		'mysql' => array(),
+		'debug' => 1,
+		'mysql' => array(
+
+            'MYSQL_HOST' => 'localhost',
+            'MYSQL_PORT' => '3306',
+            'MYSQL_USER' => 'root',
+            'MYSQL_DB'   => 'speedphp_test',
+            'MYSQL_PASS' => '',
+            'MYSQL_CHARSET' => 'utf8',
+        ),
 	),
 );
-// 为了避免开始使用时会不正确配置域名导致程序错误，加入判断
 if(empty($domain[$_SERVER["HTTP_HOST"]])) die("配置域名不正确，请确认".$_SERVER["HTTP_HOST"]."的配置是否存在！");
 
 return $domain[$_SERVER["HTTP_HOST"]] + $config;
--- protected/controller/MainController.php
+++ protected/controller/MainController.php
@@ -2,13 +2,16 @@
 class MainController extends BaseController {
 	// 首页
 	function actionIndex(){
+	    $guest_book = new Model("guestbook");
+//	    $this->records = $guest_book->findAll();
+//	    var_dump($this->records);
 	    //渲染页面
         $this->display("guestbook.html");
 	}
 
 	function actionWrite()
     {
-//        print_r(arg());die;
+//        dump(arg());die;
         //数据整理
         $new_form = [
             'title'      => arg('title'),
@@ -19,6 +22,7 @@ class MainController extends BaseController {
 
         //实例化model
         $guest_model = new Model('guestbook');
+
         //使用model中的create方法将前面的数据插入表中
         $result = $guest_model->create($new_form);
 
@@ -29,6 +33,7 @@ class MainController extends BaseController {
         }
         $this->tips("留言成功！", url("main", "index"));
     }
-	
+
+
 
 }
